2ff759a320fa548f1802dab1de5b3bcd14e6c072
Anton Schirg <anton.schirg@posteo.de>
2024-11-17 01:48:20 -0500

    multi applet test for clock source and sink

--- software/glasgow/applet/audio/tdm/__init__.py
+++ software/glasgow/applet/audio/tdm/__init__.py
@@ -9,16 +9,18 @@ from ....gateware.clockgen import ClockGen
 
 class TDMBus(Elaboratable):
     def __init__(self, ports, clock_dir):
-        self.tx_buffer = io.Buffer("o", ports.tx)
-        self.rx_buffer = io.Buffer("i", ports.rx)
+        self.tx_buffer = io.Buffer("o", ports.tx) if hasattr(ports, 'tx') else None
+        self.rx_buffer = io.Buffer("i", ports.rx) if hasattr(ports, 'rx') else None
         dir = 'o' if clock_dir == 'source' else 'i'
         self.bclk_buffer = io.Buffer(dir, ports.bclk)
         self.fsync_buffer = io.Buffer(dir, ports.fsync)
     
     def elaborate(self, platform):
         m = Module()
-        m.submodules.tx_buffer = self.tx_buffer
-        m.submodules.rx_buffer = self.rx_buffer
+        if self.tx_buffer:
+            m.submodules.tx_buffer = self.tx_buffer
+        if self.rx_buffer:
+            m.submodules.rx_buffer = self.rx_buffer
         m.submodules.bclk_buffer = self.bclk_buffer
         m.submodules.fsync_buffer = self.fsync_buffer
         return m
@@ -46,15 +48,13 @@ class TDMSubtarget(Elaboratable):
         bits_per_frame = self.n_channels * self.n_bits
         bytes_per_frame = self.n_channels * self.n_bits // 8
 
-        m.domains.bclk = cd_bclk = ClockDomain("bclk")
+        m.domains.bclk = cd_bclk = ClockDomain("bclk", local=True)
         fsync = Signal()
 
         # What happens if bclk and fsync are not perfectly synchonized?
 
         # TODO detect buffer underflows / overflows and indicate error
         # TODO detect errors such as too early fsync
-
-        # TODO allow disabling one of rx and tx
 
         fsync_counter = Signal(range(bits_per_frame * 4))
 
@@ -64,8 +64,12 @@ class TDMSubtarget(Elaboratable):
             m.submodules.clockgen = ClockGen(self.bclk_cyc)
             m.d.comb += cd_bclk.clk.eq(m.submodules.clockgen.clk)
 
+            # TODO add an enable that starts the clocks only once data is ready in the queue
+            # and maybe also stop them once we are done
+
             frame_duration = bits_per_frame + self.extra_cycles
             
+            # If we are the clock source fsync is driven by the fsync counter
             with m.If(fsync_counter < frame_duration - 1):
                 m.d.bclk += fsync_counter.eq(fsync_counter + 1)
             with m.Else():
@@ -78,37 +82,42 @@ class TDMSubtarget(Elaboratable):
             assert self.clock_dir == 'sink'
             m.d.comb += cd_bclk.clk.eq(self.bus.bclk_buffer.i)
             m.d.comb += fsync.eq(self.bus.fsync_buffer.i)
-            with m.If(fsync):
-                # TODO this should happen immediately, not after one bclk
-                m.d.bclk += fsync_counter.eq(0)
-                # TODO could report number of bits per fsync here
-            with m.ElIf(fsync_counter < 2**fsync_counter.width-2):
-                m.d.bclk += fsync_counter.eq(fsync_counter + 1)
+
+            # If we are the clock sink the fsync_counter is driven by fsync
+            # TODO this should happen immediately, not after one bclk
+            m.d.bclk += fsync_counter.eq(Mux(fsync, 0, Mux(fsync_counter < 2**len(fsync_counter)-2, fsync_counter + 1, fsync_counter)))
+            # TODO could report number of bits per fsync here
+            # TODO Also doesn't behave correctly with fsync longer that 1 bclk, should use the fsync_rising signal
 
         # do we actually need those or could we use self.out_fifo._fifo.r_level? self.out_fifo might not be async though
 
         # fifos for transfer between sync domain and bclk domain
-        m.submodules.o_fifo = o_fifo = fifo.AsyncFIFO(width=8, depth=bytes_per_frame, r_domain='bclk', w_domain='sync')
-        m.submodules.i_fifo = i_fifo = fifo.AsyncFIFO(width=8, depth=bytes_per_frame, r_domain='sync', w_domain='bclk')
+        
+        if self.bus.tx_buffer != None:
+            m.submodules.o_fifo = o_fifo = fifo.AsyncFIFO(width=8, depth=bytes_per_frame, r_domain='bclk', w_domain='sync')
+            # Fill o_fifo from self.out_fifo
+            with m.If(o_fifo.w_rdy & self.out_fifo.r_rdy):
+                m.d.comb += o_fifo.w_en.eq(1)
+                m.d.comb += self.out_fifo.r_en.eq(1)
+                m.d.sync += o_fifo.w_data.eq(self.out_fifo.r_data) # TODO why is this one sync, but in the other direction comb? Works, but suspicious
 
-        # Fill o_fifo from self.out_fifo
-        with m.If(o_fifo.w_rdy & self.out_fifo.r_rdy):
-            m.d.comb += o_fifo.w_en.eq(1)
-            m.d.comb += self.out_fifo.r_en.eq(1)
-            m.d.sync += o_fifo.w_data.eq(self.out_fifo.r_data) # TODO why is this one sync, but in the other direction comb? Works, but suspicious
+            full_frame_in_fifo = Signal()
+            m.d.comb += full_frame_in_fifo.eq(m.submodules.o_fifo.r_level >= bytes_per_frame - 2) # -2 because we keep 1 byte in the fifo out register, I think. And one somewhere else? Maybe I have to actually think this through
+        else:
+            full_frame_in_fifo = Const(1)
 
-        # Empty i_fifo into self.in_fifo
-        with m.If(i_fifo.r_rdy & self.in_fifo.w_rdy):
-            m.d.comb += i_fifo.r_en.eq(1)
-            m.d.comb += self.in_fifo.w_en.eq(1)
-            m.d.comb += self.in_fifo.w_data.eq(i_fifo.r_data)
+        if self.bus.rx_buffer != None:
+            m.submodules.i_fifo = i_fifo = fifo.AsyncFIFO(width=8, depth=bytes_per_frame, r_domain='sync', w_domain='bclk')
+            # Empty i_fifo into self.in_fifo
+            with m.If(i_fifo.r_rdy & self.in_fifo.w_rdy):
+                m.d.comb += i_fifo.r_en.eq(1)
+                m.d.comb += self.in_fifo.w_en.eq(1)
+                m.d.comb += self.in_fifo.w_data.eq(i_fifo.r_data)
 
-        full_frame_in_fifo = Signal()
-        m.d.comb += full_frame_in_fifo.eq(m.submodules.o_fifo.r_level >= bytes_per_frame - 2) # -2 because we keep 1 byte in the fifo out register, I think. And one somewhere else? Maybe I have to actually think this through
-
-        space_for_full_frame_in_fifo = Signal()
-        m.d.comb += space_for_full_frame_in_fifo.eq(m.submodules.i_fifo.w_level >= 0)
-
+            space_for_full_frame_in_fifo = Signal()
+            m.d.comb += space_for_full_frame_in_fifo.eq(m.submodules.i_fifo.w_level == 0)
+        else:
+            space_for_full_frame_in_fifo = Const(1)
 
         # Limit fsync length to one bclk internally
         fsync_reg = Signal()
@@ -127,7 +136,7 @@ class TDMSubtarget(Elaboratable):
         # If output_fsync_delay is set to zero we want full_frame_in_fifo to immediately set active_frame, not after one bclk
         active_frame_imm = Signal()
         if self.output_fsync_delay == 0:
-            m.d.comb += active_frame_imm.eq(Mux(fsync_rising, full_frame_in_fifo, active_frame))
+            m.d.comb += active_frame_imm.eq(Mux(fsync_rising, full_frame_in_fifo & space_for_full_frame_in_fifo, active_frame))
             # TODO active_frame_imm maybe also has to deassert one bclk earlier than active_frame
         elif self.output_fsync_delay == 1:
             m.d.comb += active_frame_imm.eq(active_frame)
@@ -140,50 +149,58 @@ class TDMSubtarget(Elaboratable):
         m.d.comb += still_shifting.eq(fsync_counter < bits_per_frame + self.output_fsync_delay)
 
         # TODO detect if fsync comes before bits per frame is reached
+        # In that case we have to fill the fifo with zeros
+        # Or have a mechanism to drop a frame before we send it out
 
-        bits_valid_o = Signal(range(9))
-        bits_valid_i = Signal(range(9))
-        shreg_o = Signal(8)
-        shreg_i = Signal(8)
-        m.d.comb += self.bus.tx_buffer.o.eq(shreg_o[-1] & active_frame_imm & still_shifting)
-        
 
-        # Shift data out of shreg_o and into shreg_i on every bclk
-        with m.If(active_frame_imm & still_shifting):
-            m.d.bclk += shreg_o.eq(Cat(C(0, 1), shreg_o))
-            m.d.bclk += shreg_i.eq(Cat(self.bus.rx_buffer.i, shreg_i))
-            m.d.bclk += bits_valid_o.eq(bits_valid_o - 1)
-            m.d.bclk += bits_valid_i.eq(bits_valid_i + 1)
-        
-        # Send out received byte
-        with m.If(bits_valid_i >= 8):
-            m.d.bclk += i_fifo.w_data.eq(shreg_i)
-            m.d.bclk += i_fifo.w_en.eq(1)
-            with m.If(active_frame_imm & still_shifting):  # If we also shifted in data in this cycle
-                m.d.bclk += bits_valid_i.eq(1)
+        if self.bus.tx_buffer != None:
+            bits_valid_o = Signal(range(9))
+            shreg_o = Signal(8)
+            m.d.comb += self.bus.tx_buffer.o.eq(shreg_o[-1] & active_frame_imm & still_shifting)
+
+            # Shift data out of shreg_o and into shreg_i on every bclk
+            with m.If(active_frame_imm & still_shifting):
+                m.d.bclk += shreg_o.eq(Cat(C(0, 1), shreg_o))
+                m.d.bclk += bits_valid_o.eq(bits_valid_o - 1)
+            
+            # Get new transmit byte
+            #TODO did I misunderstand fifo docs here? r_rdy means that data is already in r_data and r_en will put in the next one
+            # or does it? I see r_rdy asserted and r_data contains nothing
+            # maybe this mistake and the comb sync thing above cancel each other?
+            # maybe this can be simplified by fixing both
+            with m.FSM(name="shreg_o_refill", domain="bclk"):
+                with m.State("Wait"):
+                    # Read a byte from the fifo as soon as it is available, it will be kept in r_data until we need it
+                    with m.If(o_fifo.r_rdy):
+                        m.d.comb += o_fifo.r_en.eq(1)
+                        m.next = "Read"
+                with m.State("Read"):
+                    # When the shift register is empty refill it from r_data
+                    with m.If(bits_valid_o <= 1):
+                        m.d.bclk += shreg_o.eq(o_fifo.r_data)
+                        m.d.bclk += bits_valid_o.eq(8)
+
+                        m.next = "Wait"
+
+        if self.bus.rx_buffer != None:
+            bits_valid_i = Signal(range(9))
+            shreg_i = Signal(8)
+
+            with m.If(active_frame_imm & still_shifting):
+                m.d.bclk += shreg_i.eq(Cat(self.bus.rx_buffer.i, shreg_i))
+                m.d.bclk += bits_valid_i.eq(bits_valid_i + 1)
+            
+            # Send out received byte
+            with m.If(bits_valid_i >= 8):
+                m.d.bclk += i_fifo.w_data.eq(shreg_i)
+                m.d.bclk += i_fifo.w_en.eq(1)
+                with m.If(active_frame_imm & still_shifting):  # If we also shifted in data in this cycle
+                    m.d.bclk += bits_valid_i.eq(1)
+                with m.Else():
+                    m.d.bclk += bits_valid_i.eq(0)
             with m.Else():
-                m.d.bclk += bits_valid_i.eq(0)
-        with m.Else():
-            m.d.bclk += i_fifo.w_en.eq(0)
+                m.d.bclk += i_fifo.w_en.eq(0)
         
-        # Get new transmit byte
-        #TODO did I misunderstand fifo docs here? r_rdy means that data is already in r_data and r_en will put in the next one
-        # maybe this mistake and the comb sync thing above cancel each other?
-        # maybe this can be simplified by fixing both
-        with m.FSM(name="shreg_o_refill", domain="bclk"):
-            with m.State("Wait"):
-                # Read a byte from the fifo as soon as it is available, it will be kept in r_data until we need it
-                with m.If(o_fifo.r_rdy):
-                    m.d.comb += o_fifo.r_en.eq(1)
-                    m.next = "Read"
-            with m.State("Read"):
-                # When the shift register is empty refill it from r_data
-                with m.If(bits_valid_o <= 1):
-                    m.d.bclk += shreg_o.eq(o_fifo.r_data)
-                    m.d.bclk += bits_valid_o.eq(8)
-
-                    m.next = "Wait"
-
 
         return m
 
@@ -227,7 +244,7 @@ class TDMApplet(GlasgowApplet):
             "--extra-cycles", type=int, default=0,
             help="number of bclk cycles after a frame is sent before the next fsync pulse in generated (default: %(default)s)")
 
-    def build(self, target, args):
+    def build(self, target, args, name=None):
         assert args.bit_depth % 8 == 0
         bclk_frequency = args.sample_rate * args.channels * args.bit_depth
         bclk_cyc = self.derive_clock(input_hz=target.sys_clk_freq, output_hz=bclk_frequency)
@@ -235,14 +252,16 @@ class TDMApplet(GlasgowApplet):
 
         assert args.fsync_duration > 0 and args.fsync_duration < args.channels * args.bit_depth
 
+        ports = {
+            'rx': args.pin_rx,
+            'tx': args.pin_tx,
+            'bclk': args.pin_bclk,
+            'fsync': args.pin_fsync,
+        }
+
         self.mux_interface = iface = target.multiplexer.claim_interface(self, args)
         subtarget = iface.add_subtarget(TDMSubtarget(
-            ports=iface.get_port_group(
-                rx=args.pin_rx,
-                tx=args.pin_tx,
-                bclk=args.pin_bclk,
-                fsync=args.pin_fsync,
-            ),
+            ports=iface.get_port_group(**{k:v for k,v in ports.items() if v is not None}),
             out_fifo=iface.get_out_fifo(),
             in_fifo=iface.get_in_fifo(),
             clock_dir=args.clock_dir,
--- software/glasgow/applet/audio/tdm/test.py
+++ software/glasgow/applet/audio/tdm/test.py
@@ -1,15 +1,64 @@
-import types
 from amaranth import *
-from amaranth.lib import io
+from amaranth.sim import Simulator
+import unittest
+import argparse
+import functools
+import types
 
 from ... import *
 from . import TDMApplet
+from ....access.simulation.arguments import SimulationArguments
+
+from ....access.simulation import *
+from ....target.simulation import *
+from ....device.simulation import *
+
+
+
+def multi_applet_simulation_test(setup):
+    def decorator(case):
+        @functools.wraps(case)
+        def wrapper(self):
+
+            getattr(self, setup)()
+            @types.coroutine
+            def run():
+                yield from case(self)
+
+            sim = Simulator(self.target)
+            sim.add_clock(1e-9)
+            sim.add_sync_process(run)
+            vcd_name = f"{case.__name__}.vcd"
+            with sim.write_vcd(vcd_name):
+                sim.run()
+            print(vcd_name)
+            # os.remove(vcd_name)
+
+        return wrapper
+
+    return decorator
+
+class MultiAppletTestCase(unittest.TestCase):
+    def _prepare_simulation_target(self):
+        self.target = GlasgowSimulationTarget(multiplexer_cls=SimulationMultiplexer)
+
+        self.device = GlasgowSimulationDevice(self.target)
+        self.device.demultiplexer = SimulationDemultiplexer(self.device)
+
+    def _parse_applet_args(self, applet, args, interact=False):
+        access_args = SimulationArguments(applet)
+        parser = argparse.ArgumentParser()
+        applet.add_build_arguments(parser, access_args)
+        applet.add_run_arguments(parser, access_args)
+        if interact:
+            applet.add_interact_arguments(parser)
+        return parser.parse_args(args)
 
 
 class TDMAppletTestCase(GlasgowAppletTestCase, applet=TDMApplet):
     @synthesis_test
     def test_build(self):
-        self.assertBuilds(args=[])
+        self.assertBuilds(args=['--pin-bclk', "0", '--pin-fsync', "1", '--pin-rx', "2", '--pin-tx', "3"])
 
     def setup_loopback(self):
         self.build_simulated_applet()
@@ -19,9 +68,8 @@ class TDMAppletTestCase(GlasgowAppletTestCase, applet=TDMApplet):
         m.d.comb += ports.rx.i.eq(ports.tx.o)
         self.target.add_submodule(m)
 
-    @applet_simulation_test("setup_loopback", [])
+    @applet_simulation_test("setup_loopback", ['--pin-bclk', "0", '--pin-fsync', "1", '--pin-rx', "2", '--pin-tx', "3"])
     async def test_loopback(self):
-        mux_iface = self.applet.mux_interface
         tdm_iface = await self.run_simulated_applet()
 
         test_data = b"\xFF\x00\x01\x02" * 2
@@ -35,3 +83,43 @@ class TDMAppletTestCase(GlasgowAppletTestCase, applet=TDMApplet):
         self.assertEqual(result, test_data)
 
         # TODO test buffer underflow
+
+# This is currently not loaded by `glasgow test` but can be run using `python -m unittest`
+class TDMMultiAppletTestCase(MultiAppletTestCase):
+
+    def setup_source_sink(self):
+        self._prepare_simulation_target()
+
+        args_source = ['--pin-bclk', "0", '--pin-fsync', "1", '--pin-tx', "2", '--clock-dir', 'source', '--fsync-duration', "5"]
+        args_sink = ['--pin-bclk', "4", '--pin-fsync', "5", '--pin-rx', "6", '--clock-dir', 'sink']
+
+        self.source_applet = TDMApplet()
+        self.source_parsed_args = self._parse_applet_args(self.source_applet, args_source)
+        subtarget_source = self.source_applet.build(self.target, self.source_parsed_args, name='source_applet')
+
+        self.sink_applet = TDMApplet()
+        self.sink_parsed_args = self._parse_applet_args(self.sink_applet, args_sink)
+        subtarget_sink = self.sink_applet.build(self.target, self.sink_parsed_args, name='sink_applet')
+
+        source_ports = subtarget_source.ports
+        sink_ports = subtarget_sink.ports
+
+        m = Module()
+        m.d.comb += sink_ports.rx.i.eq(source_ports.tx.o)
+        m.d.comb += sink_ports.bclk.i.eq(source_ports.bclk.o)
+        m.d.comb += sink_ports.fsync.i.eq(source_ports.fsync.o)
+        self.target.add_submodule(m)
+
+    @multi_applet_simulation_test("setup_source_sink")
+    async def test_source_sink(self):
+        source_iface = await self.source_applet.run(self.device, self.source_parsed_args)
+        sink_iface = await self.sink_applet.run(self.device, self.sink_parsed_args)
+
+        empty_frame = b"\x00" * 4
+        test_data = b"\xFF\x00\x01\x02" * 2
+
+        await source_iface.write(test_data)
+
+        # The first frame is read as zeros because the source needs a moment to get going
+        result = await sink_iface.read(len(empty_frame + test_data))
+        self.assertEqual(result, empty_frame + test_data)
