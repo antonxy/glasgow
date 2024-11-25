b4035ab6d138c163d85aa14f5aade6898b8ee53a
Anton Schirg <anton.schirg@posteo.de>
2024-11-24 23:23:24 -0500

    Add hardware two applet test

--- software/glasgow/applet/audio/tdm/test.py
+++ software/glasgow/applet/audio/tdm/test.py
@@ -4,6 +4,8 @@ import unittest
 import argparse
 import functools
 import types
+import asyncio
+import threading
 
 from ... import *
 from . import TDMApplet
@@ -12,6 +14,9 @@ from ....access.simulation.arguments import SimulationArguments
 from ....access.simulation import *
 from ....target.simulation import *
 from ....device.simulation import *
+from glasgow.target.hardware import GlasgowHardwareTarget
+from glasgow.device.hardware import GlasgowHardwareDevice
+from glasgow.access.direct import DirectMultiplexer, DirectDemultiplexer, DirectArguments
 
 
 
@@ -37,6 +42,37 @@ def multi_applet_simulation_test(setup):
         return wrapper
 
     return decorator
+
+
+def multi_applet_hardware_test():
+    def decorator(case):
+        @functools.wraps(case)
+        def wrapper(self):
+            exception = None
+            def run_test():
+                try:
+                    loop = asyncio.new_event_loop()
+                    loop.run_until_complete(case(self))
+
+                except Exception as e:
+                    nonlocal exception
+                    exception = e
+
+                finally:
+                    if self.device is not None:
+                        loop.run_until_complete(self.device.demultiplexer.cancel())
+                    loop.close()
+
+            thread = threading.Thread(target=run_test)
+            thread.start()
+            thread.join()
+            if exception is not None:
+                raise exception
+
+        return wrapper
+
+    return decorator
+
 
 class MultiAppletTestCase(unittest.TestCase):
     def _prepare_simulation_target(self):
@@ -101,6 +137,9 @@ class TDMAppletTestCase(GlasgowAppletTestCase, applet=TDMApplet):
     async def test_loopback4(self):
         await self.check_loopback(frame_size=9)
 
+    
+    # TODO test buffer underflow
+    # TODO test with recorded traces. Problem: looks like there is no vcd reader available in the Simulator
 
 # This is currently not loaded by `glasgow test` but can be run using `python -m unittest`
 class TDMMultiAppletTestCase(MultiAppletTestCase):
@@ -108,7 +147,7 @@ class TDMMultiAppletTestCase(MultiAppletTestCase):
     def setup_source_sink(self):
         self._prepare_simulation_target()
 
-        args_source = ['--pin-bclk', "0", '--pin-fsync', "1", '--pin-tx', "2", '--clock-dir', 'source', '--fsync-duration', "5"]
+        args_source = ['--pin-bclk', "0", '--pin-fsync', "1", '--pin-tx', "2", '--clock-dir', 'source', '--fsync-duration', "5", '--extra-cycles', '3']
         args_sink = ['--pin-bclk', "4", '--pin-fsync', "5", '--pin-rx', "6", '--clock-dir', 'sink']
 
         self.source_applet = TDMApplet()
@@ -141,3 +180,51 @@ class TDMMultiAppletTestCase(MultiAppletTestCase):
         # The first frame is read as zeros because the source needs a moment to get going
         result = await sink_iface.read(len(empty_frame + test_data))
         self.assertEqual(result, empty_frame + test_data)
+
+
+    @multi_applet_hardware_test()
+    async def test_hardware_multi_applet(self):
+        device = GlasgowHardwareDevice()
+        self.device = device
+        await device.reset_alert("AB")
+        await device.poll_alert()
+        await device.set_voltage("AB", 3.3)
+        target = GlasgowHardwareTarget(revision=device.revision,
+                                        multiplexer_cls=DirectMultiplexer,
+                                        with_analyzer=False)
+        access_args = DirectArguments(applet_name="tdm",
+                                        default_port="AB",
+                                        pin_count=16)
+        tdm0_parser = argparse.ArgumentParser('tdm0')
+        tdm1_parser = argparse.ArgumentParser('tdm1')
+        TDMApplet.add_build_arguments(tdm0_parser, access_args)
+        TDMApplet.add_build_arguments(tdm1_parser, access_args)
+        TDMApplet.add_run_arguments(tdm0_parser, access_args)
+        TDMApplet.add_run_arguments(tdm1_parser, access_args)
+        TDMApplet.add_interact_arguments(tdm0_parser)
+        TDMApplet.add_interact_arguments(tdm1_parser)
+
+        # Assumming ping 0,1,2 are connected to 8,9,10 respectively
+        tdm0_args = tdm0_parser.parse_args(["-V", "3.3", '--pin-bclk', "0", '--pin-fsync', "1", '--pin-tx', "2", '--clock-dir', 'source'])
+        tdm1_args = tdm0_parser.parse_args(["-V", "3.3", '--pin-bclk', "8", '--pin-fsync', "9", '--pin-rx', "10", '--clock-dir', 'sink'])
+        tdm0 = TDMApplet()
+        tdm1 = TDMApplet()
+        tdm0.build(target, tdm0_args)
+        tdm1.build(target, tdm1_args)
+        plan = target.build_plan()
+        await device.download_target(plan)
+        device.demultiplexer = DirectDemultiplexer(device, target.multiplexer.pipe_count)
+
+        iface1 = await tdm1.run(device, tdm1_args) # Start receiver first so that it's ready before sender starts sending
+        iface0 = await tdm0.run(device, tdm0_args)
+        await iface1.read() # Clear the receiver from previous runs
+
+        test_data = b'abcdabceabcfabcg' * 100
+
+        await iface0.write(test_data)
+        await iface0.flush(wait=False)
+
+        # TODO a bunch of zeros are received before the actual data.
+        # The clock source should maybe wait to enable clock until it's fifo is somewhat filled
+        result = bytes(await iface1.read(len(test_data) + 500))
+        self.assertIn(test_data, result)
